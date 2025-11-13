1fc750b804d5dfd69acd165fe16e65239febdbc1
guteeeeeeeee <gutierrezjosemaria01@hotmail.com>
2025-11-13 16:25:29 -0300

    actividad 6

--- app/components/pokemon_list.tsx
+++ app/components/pokemon_list.tsx
@@ -1,14 +1,43 @@
 "use client";
 
 import Link from "next/link";
-
+import {useQuery} from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import PokemonItem from "./pokemon_item";
 
-type PokemonBase = { name: string; url: string };
+type Pokemon={
+	name:string,
+	url:string;
+};
 
-export default function PokemonList() {
+async function fetchPokemons(): Promise<Pokemon[]>{
+	const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
+	return res.json();
+}
+
+export default function PokemonList(){
+	const {data,isLoading,error} = useQuery<Pokemon[]>({
+		queryKey:["pokemon"],
+		queryFn: fetchPokemons,
+	});
+	
+	if (isLoading) return <p>Cargando...</p>;
+	if (error) return <p>Error al cargar</p>;
+	
+	return (
+		<ul>
+			{data.results?.map((p,index)=>(
+				<li key={index}>
+					<PokemonItem name={p.name} url={p.url} />
+        	 	 	<Link href={`/pokemon/${index + 1}`}>{p.name}</Link>
+				</li>
+			))}
+		</ul>
+	);
+}
+
+/*export default function PokemonList() {
   const [pokemons, setPokemons] = useState<PokemonBase[]>([]);
 
   useEffect(() => {
@@ -27,4 +56,4 @@ export default function PokemonList() {
       ))}
     </ul>
   );
-}
+}*/
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+
 import Link from "next/link";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children, }: { children: React.ReactNode; }) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <html lang="es">
       <body>
-        <header>
-          <nav>
-          	<Link href="/">Inicio</Link>
-          </nav>
-        </header>
+		<QueryClientProvider client={queryClient}>
+      		<header>
+          		<nav>
+          			<Link href="/">Inicio</Link>
+          		</nav>
+        	</header>
 
-        <main>{children}</main>
+        	<main>{children}</main>
 
-        <footer>Proyecto Distribuidos</footer>
+        	<footer>Proyecto Distribuidos</footer>
+        </QueryClientProvider>
       </body>
     </html>
   );
